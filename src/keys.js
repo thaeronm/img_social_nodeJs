f18395a3b0715936c3537a755fd1bfb49eae3d10
Thaeron Morales <thaeron.vash@gmail.com>
2020-04-11 14:33:50 -0400

    bd

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
     database: {
-        URI: 'mongodb+srv://thaeronm:thaeronm@cluster0-xfjga.mongodb.net/test?retryWrites=true&w=majority
-        '
+        URI: 'mongodb+srv://thaeronm:thaeronm@cluster0-xfjga.mongodb.net/test?retryWrites=true&w=majority',
     }
 };
